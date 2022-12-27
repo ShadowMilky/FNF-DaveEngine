351052916f681eeda5e4ccf0d16cdae0f5829078
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-12-27 18:33:14 -0500

    newer foxa sprites

--- source/Character.hx
+++ source/Character.hx
@@ -318,6 +318,23 @@ class Character extends FlxSprite
 				barColor = FlxColor.fromString(getColorCode(curCharacter));
 
 				playAnim('idle');
+			case 'foxa-newer': // foxa regular newer
+				frames = Paths.getSparrowAtlas('characters/goofy_ahh_foxa', 'shared');
+
+				animation.addByPrefix('idle', 'FoxaIdle', 24, false);
+				animation.addByPrefix('singUP', 'Foxa up', 24, false);
+				animation.addByPrefix('singRIGHT', 'Foxa right', 24, false);
+				animation.addByPrefix('singDOWN', 'Foxa down', 24, false);
+				animation.addByPrefix('singLEFT', 'Foxa left', 24, false);
+
+				loadOffsetFile(curCharacter);
+
+				barColor = FlxColor.fromString(getColorCode(curCharacter));
+
+				setGraphicSize(Std.int(width * 0.8));
+				updateHitbox();
+
+				playAnim('idle');
 			case 'foxa-new': // foxa regular new
 				frames = Paths.getSparrowAtlas('characters/FoxaNewSprite', 'shared');
 
