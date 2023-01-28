272af371e8693c48a43e1960831a4153ee30dca5
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2023-01-28 13:24:04 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -34,7 +34,7 @@ class Main extends Sprite
 
 	public static var framerate:Int = 144; // How many frames per second the game should run at.
 
-	var skipSplash:Bool = false; // Whether to skip the flixel splash screen that appears in release mode.
+	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
 	public static var fps:FpsDisplay;
