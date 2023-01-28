273283ba4dbc409305066ef29ea43329e82d4dac
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2023-01-28 13:26:47 -0500

    lol

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -219,7 +219,7 @@ class MainMenuState extends MusicBeatState
 		firstStart = false;
 
 		// var versionShit:FlxText = new FlxText(1, FlxG.height - 45, FlxG.width, '${daRealEngineVer} Engine v${engineVer}\nFNF v${gameVer}', 12);
-		var versionShit:FlxText = new FlxText(1, FlxG.height - 45, FlxG.width, 'Vs. Foxa Plus ALPHA\nFNF v${gameVer}', 12);
+		var versionShit:FlxText = new FlxText(1, FlxG.height - 45, FlxG.width, 'Vs. Foxa Expanded ALPHA\nFNF v${gameVer}', 12);
 		versionShit.antialiasing = true;
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
--- source/SelectLanguageState.hx
+++ source/SelectLanguageState.hx
@@ -36,7 +36,7 @@ class SelectLanguageState extends MusicBeatState
 		bg.color = langaugeList[curLanguageSelected].langaugeColor;
 		add(bg);
 
-		selectLanguage = new FlxText(0, (FlxG.height / 2) - 300, FlxG.width, "Please Select a Language", 45);
+		selectLanguage = new FlxText(0, (FlxG.height / 2) - 300, FlxG.width, "Welcome!\nPlease Select a Language.", 45);
 		selectLanguage.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		selectLanguage.antialiasing = true;
 		selectLanguage.borderSize = 2;
@@ -88,7 +88,7 @@ class SelectLanguageState extends MusicBeatState
 			{
 				accepted = true;
 
-				trace('LETS GO');
+				trace('language selected\nstarting game...');
 
 				FlxG.sound.play(Paths.sound('confirmMenu'), 0.4);
 
